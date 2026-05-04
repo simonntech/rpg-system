83ab027cea9e3ca048b6f74351e3c6ed027f90c7
SIMON TECH - Bruno Simon <contato.simon.tech@gmail.com>
2026-05-04 20:36:48 -0300

    Updates

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-export default function Button({ text, onclick }: { text: string; onclick: () => void }) {
-  return (
-    <div className="m-2">
-      <button className="bg-black text-white px-4 py-7 mb-8 font-black cursor-pointer rounded-2xl hover:bg-amber-100 hover:text-black transition-colors duration-300" onClick={onclick} type="submit">
-        {text}
-      </button>
-    </div>
-  );
-}
--- src/components/SecondaryButton.tsx
+++
@@ -1,9 +0,0 @@
-export default function SecondaryButton({ text, onclick }: { text: string; onclick: () => void }) {
-  return (
-    <div className="m-2">
-      <button className="bg-amber-100 text-black px-4 py-7 mb-8 font-black cursor-pointer rounded-2xl hover:bg-black hover:text-white transition-colors duration-300" onClick={onclick}>
-        {text}
-      </button>
-    </div>
-  );
-}
